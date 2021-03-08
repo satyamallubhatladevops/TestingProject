352b64e62a1b7a1145cfd77b3c0c999057169f0a
=SatyaPrakash <= satyamallubhatladevops@gmail.com>
2021-03-08 08:39:42 +0530

    Test.java file is added by PracticeA user

---
+++ Test.java
@@ -0,0 +1,22 @@
+public class Test{
+
+public void testo1(){
+
+
+System.out.println("Test01");
+}
+
+public void testo2(){
+System.out.println("Test02");
+}
+
+public void testo3(){
+System.out.println("Test03");
+}
+
+public void testo4(){
+System.out.println("Test04");
+}
+
+
+}
